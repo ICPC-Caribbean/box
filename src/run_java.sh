3bceb04d07e7ab690a4f167adeb1b0ecf2137a70
Marcelo Fornet <mfornet94@gmail.com>
2019-04-08 01:08:19 -0400

    Init box repository
    Add all box files into one repository.
    No organization so far.

---
+++ src/run_java.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#java -Xms512m -Xmx512m -Xss20m -cp `dirname $0` `basename $0 .jexe`
+#java -Xms1024m -Xmx1024m -Xss20m -cp `dirname $0` `basename $0 .exe` $*
+java -Xms1024m -Xmx1024m -Xss40m -cp `dirname $0` `basename $0 .exe` $*
